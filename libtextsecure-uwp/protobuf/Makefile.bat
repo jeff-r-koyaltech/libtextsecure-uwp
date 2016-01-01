514a0d5315c9026aab1ca41c91ebac736c5af499
Jeff R <langboost@gmail.com>
2016-01-01 17:56:52 +0530

    Making protobuf Makefile.bat work for any user. Locking Google Protobuf version to 2.4.1.555 with environment variable to help it stand out for future version upgrade changes.

--- libtextsecure-uwp/protobuf/Makefile.bat
+++ libtextsecure-uwp/protobuf/Makefile.bat
@@ -1,5 +1,6 @@
 setlocal
-SET PATH=%PATH%;C:\Users\simon\.nuget\packages\Google.ProtocolBuffers\2.4.1.555\tools
+SET GOOGLEPROTOVER=2.4.1.555
+SET PATH=%PATH%;%USERPROFILE%\.nuget\packages\Google.ProtocolBuffers\%GOOGLEPROTOVER%\tools
 ProtoGen -namespace="libtextsecure.websocket" -umbrella_classname="WebSocketProtos" -nest_classes=true  -output_directory="../websocket/" WebSocketResources.proto
 REM ProtoGen -namespace="libtextsecure.push" -umbrella_classname="PushMessageProtos" -nest_classes=true  -output_directory="../push/" IncomingPushMessageSignal.proto 
 ProtoGen -namespace="libtextsecure.push" -umbrella_classname="TextSecureProtos" -nest_classes=true  -output_directory="../push/" Textsecure.proto 
